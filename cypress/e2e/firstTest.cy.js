ab37232c7a80086059b21828635731575c08317c
TestKD1 <testerkamila@gmail.com>
2022-12-18 18:21:57 +0100

    udemy lessons 1-11

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+//const { describe, it, beforeEach } = require("node:test");
+
+
+describe('Our first suite', () =>{
+
+
+        it('first test', () =>{
+
+            cy.visit('/')
+            cy.contains('Forms').click()
+            cy.contains('Form Layout').click()
+          
+            // by Tag Name
+            cy.get('input')
+
+            // by Tag ID
+            cy.get('#inputEmail')
+
+            // by Calss name
+            cy.get('.input-full-width')
+
+            // by Attribute name
+            cy.get('[placeholder]')
+
+            // by Attribute name and value
+            cy.get('[placeholder="Email"]')
+
+            // by Class value
+            cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+            // by Tag name and Attribute with value
+            cy.get('input[placeholder="Email"]')
+
+            // by two different attributes
+            cy.get('[placeholder="Email"][fullwidth]')
+
+             // by tag name, Attribute with value, ID and Class name
+             cy.get('input[placeholder="Email"]#inputEmail.input-full-width')
+
+             // The most recommended way by Cypress
+             cy.get('[data-cy="imputEmail1"]')
+
+        
+
+         })
+
+         
+
+
+ 
+})
