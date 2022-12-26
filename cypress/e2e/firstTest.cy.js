4e1a3c330a33ad5f842050fa80e3a68ffd5afbb3
TestKD1 <testerkamila@gmail.com>
2022-12-26 23:30:18 +0100

    lesson 14 (invoke)

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -69,16 +69,16 @@ describe('Our first suite', () =>{
                 .find('[type="email"]')
          })
 
-    it.only('then and wrap methods', () =>{
-        cy.visit('/')
-        cy.contains('Forms').click()
-        cy.contains('Form Layout').click()
-        /*
-        cy.contains('nb-card','Using the Grid')
+        it('then and wrap methods', () =>{
+            cy.visit('/')
+            cy.contains('Forms').click()
+            cy.contains('Form Layout').click()
+            /*
+            cy.contains('nb-card','Using the Grid')
             .find('[for="inputEmail1"]')
             .should('contain','Email')
 
-        cy.contains('nb-card','Using the Grid')
+             cy.contains('nb-card','Using the Grid')
             .find('[for="inputPassword2"]')
             .should('contain','Password')
 
@@ -87,13 +87,13 @@ describe('Our first suite', () =>{
             .find('[for="exampleInputEmail1"]')
             .should('contain','Email')
 
-        cy.contains('nb-card','Basic form')
+            cy.contains('nb-card','Basic form')
             .find('[for="exampleInputPassword1"]')
             .should('contain','Password')
 
-        */
+                */
             //JQuery method
-        cy.contains('nb-card','Using the Grid').then(firstForm =>{  // rozszerzenie: function SmeName(firstForm)
+            cy.contains('nb-card','Using the Grid').then(firstForm =>{  // rozszerzenie: function SmeName(firstForm)
             const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
             const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
             expect(emailLabelFirst).to.equal('Email')
@@ -115,9 +115,38 @@ describe('Our first suite', () =>{
         })
 
 
+    })
+    it('invoke command', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layout').click()
+
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card','Basic form')
+                .find('nb-checkbox')
+                .click()
+                .find('.custom-checkbox')
+                .invoke('attr', 'class')
+                .should('contain', 'checked')
+
+
+    }) 
+    
+    it.only('assert property', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card','Common Datepicker').find('input').then(input =>{
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop','value').should('contain','Dec 17, 2022')
+        })
 
     })
-    
 
  
 })
