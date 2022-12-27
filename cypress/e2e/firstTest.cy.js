83a4da17617b010d3f8b617949bc09c37424fa2f
TestKD1 <testerkamila@gmail.com>
2022-12-27 22:31:28 +0100

    15 lesson ( checkboxes and RB)

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -135,7 +135,7 @@ describe('Our first suite', () =>{
 
     }) 
     
-    it.only('assert property', () =>{
+    it('assert property', () =>{
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
@@ -146,6 +146,42 @@ describe('Our first suite', () =>{
             cy.wrap(input).invoke('prop','value').should('contain','Dec 17, 2022')
         })
 
+    })
+
+    it('radio button', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layout').click()
+
+        cy.contains('nb-card','Using the Grid').find('[type="radio"]').then(radioButtons =>{
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+
+           cy.wrap(radioButtons)
+                .first() //.eq(0) 
+                .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+
+        })
+
+    })
+
+    it.only('check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').check({force:true})
+        //cy.get('[type="checkbox"]').eq(0).click({force:true})
     })
 
  
